df245b7f8a8a4ad1db6b11aac137c7b3cc12430a
Ankit <workxankit@gmail.com>
2025-07-05 11:36:22 +0530

    updated CreateTodo

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,10 +1,27 @@
 
 export function CreateTodo() {
     return <div>
-        <input type ="text" placeholder="title"></input> <br />
-        <input type ="text" placeholder="description"></input> <br />
+        <input style= {{
+            padding: "10px",
+            margin: "10px",
+            borderRadius: "5px",
+        }} type ="text" placeholder="title"></input> <br />
+        <input style={{
+            padding: "10px",
+            margin: "10px",
+            borderRadius: "5px",
+        }} 
+        type ="text" placeholder="description"></input> <br />
 
-        <button> Create Todo </button>
+        <button style={{
+            padding: "10px",
+            margin: "10px",
+            borderRadius: "5px",
+            backgroundColor: "#4CAF50",
+            color: "white",
+            border: "none",
+            cursor: "pointer"
+        }}> Create Todo </button>
     </div>
 }
 
